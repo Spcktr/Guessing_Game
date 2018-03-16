54b3f39b37ff29e0cdf216ac51f5a9e9805f574f
Spcktr <rocket155@gmail.com>
2018-03-16 11:36:17 +0000

    added error check for input

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     // User greeting, and input field.
     println!("Guess the number.");
     // Generate random secret
-    let secret_number = rand::thread_rng().gen_range(1, 101);
+    let secret_number = rand::thread_rng().gen_range(1, 10);
     // Call for the number and display
     println!("The secret number is: {}", secret_number);
     // loop for the game to restart under each guess
@@ -20,8 +20,10 @@ loop {
         io::stdin().read_line(&mut guess)
             .expect("Uh-oh, please enter your number again"); // Validates and returns fail text
         // converting string to rust integer and removing whitespaces
-        let guess: u32 = guess.trim().parse()
-            .expect("Please type a number.");
+        let guess: u32 = guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => continue,
+        };
             // echo user input
         println!("You guessed: {}", guess);
             // Compare input and random secret_number
